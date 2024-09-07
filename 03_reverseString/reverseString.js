d984fa3f224be1e4a31db3aa8ab382d2fdc94fcb
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-07 13:31:55 +0100

    Complete Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    
+    let returnString ="";
+for(let i = (inputString.length-1);i >= 0;i--)
+{
+    returnString += inputString[i].toString();
+}
 
+return returnString.toString();
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;

f4e8a21b1ebc522c65b3924ea5264fcd0e03b3a3
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-07 16:04:38 +0100

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let answer = 0;
 
+    if(num2<num1)
+    {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    if(!(typeof num1 === "number") || !(typeof num2 === "number")) return "ERROR";
+    if((num1 < 0) || (num2 < 0)) return "ERROR";
+    if(!(Number.isInteger(num1)) || !(Number.isInteger(num2))) return "ERROR";
+
+    do {
+        answer += num1;
+        if(num1 == num2) break;
+        num1++;
+    }
+    while(num1<=num2)
+
+return answer;
 };
 
 // Do not edit below this line

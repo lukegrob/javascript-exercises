dc578d77a6e70bda667a9aebb964e8844bf24999
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-07 16:47:10 +0100

    Complete Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(fahren) {
+    fahren = (fahren - 32) * 5/9;
+    return round(fahren,1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  celsius = (celsius * 9/5 + 32);
+  return round(celsius,1);
 };
 
 // Do not edit below this line

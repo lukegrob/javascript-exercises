ec628a927ba75c39b12a8343dbaff49cab77f63f
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-07 15:43:41 +0100

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput, ...args)
+{
+    let returnArray = [];
+
+    arrayInput.forEach((item) =>
+    {
+        if(!args.includes(item))
+        {
+            returnArray.push(item);
+        }
+    });
+
+    return returnArray;
+
 };
 
 // Do not edit below this line
